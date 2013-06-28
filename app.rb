6e43401464b06fc7b1448f373d6db9ff136e4a69
Josh Cheek <josh.cheek@gmail.com>
2013-06-28 13:07:54 -0500

    Add some more options to the form

--- app.rb
+++ app.rb
@@ -7,24 +7,37 @@ get '/' do
 end
 
 get '/fibbaaah' do
-  @nums = Fibbaaah.many_fibs_with_offset_from starts: (-5..14), offset: params[:size].to_i
+  @nums = Fibbaaah.many_fibs_with_offset_from \
+    starts: (-5..14),
+    offset: params[:size].to_i,
+    buffer: params[:buffer].to_i
+
+  @num_to_show = 5
+
   erb :result
 end
 
 __END__
 @@form
 <form action="/fibbaaah" method="get" accept-charset="utf-8">
-  <label for="size">size</label><input type="text" name="size" value="" id="size">
+
+  <label for="size">size</label>
+  <input type="text" name="size" value="16" id="size">
+
+  <label for="buffer">buffer</label>
+  <input type="text" name="buffer" value="0" id="buffer">
+
   <p><input type="submit" value="Continue &rarr;"></p>
 </form>
 
+
+
+
 @@result
 <% @nums.each do |fib_nums| %>
-  <% s4, s3, s2, s1 = fib_nums.take(4) %>
   <div>
-    <h1 style="font-size: <%= s1 %>">Header</h1>
-    <h2 style="font-size: <%= s2 %>">Header</h1>
-    <h3 style="font-size: <%= s3 %>">Header</h1>
-    <h4 style="font-size: <%= s4 %>">Header</h1>
+    <% fib_nums.take(@num_to_show).reverse.each.with_index(1) do |fib, index| %>
+      <h<%= index %> style="font-size: <%= fib %>">Header (<%= fib %>)</h1>
+    <% end %>
   </div>
 <% end %>
