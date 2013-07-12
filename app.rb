7ad5447e27a07f861d7c69d3627dd01288d3edf2
Josh Cheek <josh.cheek@gmail.com>
2013-07-12 15:22:36 -0500

    Extract inline templates to views, add layout and css

--- app.rb
+++ app.rb
@@ -16,28 +16,3 @@ get '/fibbaaah' do
 
   erb :result
 end
-
-__END__
-@@form
-<form action="/fibbaaah" method="get" accept-charset="utf-8">
-
-  <label for="size">size</label>
-  <input type="text" name="size" value="16" id="size">
-
-  <label for="buffer">buffer</label>
-  <input type="text" name="buffer" value="0" id="buffer">
-
-  <p><input type="submit" value="Continue &rarr;"></p>
-</form>
-
-
-
-
-@@result
-<% @nums.each do |fib_nums| %>
-  <div>
-    <% fib_nums.take(@num_to_show).reverse.each.with_index(1) do |fib, index| %>
-      <h<%= index %> style="font-size: <%= fib %>">Header (<%= fib %>)</h1>
-    <% end %>
-  </div>
-<% end %>
